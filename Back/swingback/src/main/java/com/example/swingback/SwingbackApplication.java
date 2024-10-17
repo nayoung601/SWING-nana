ea16ab240e48412a69450950d92af9589f8f57c3
LJongJoo <tlzhsh2@dgu.ac.kr>
2024-10-17 21:46:19 +0900

    back init set

---
+++ Back/swingback/src/main/java/com/example/swingback/SwingbackApplication.java
@@ -0,0 +1,13 @@
+package com.example.swingback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwingbackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwingbackApplication.class, args);
+	}
+
+}
